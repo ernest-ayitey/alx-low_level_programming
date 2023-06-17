775f6ca6a48ba2bf46af9e72e48f0e8af9b9a98d
ernest-ayitey <ernestayitey65@gmail.com>
2023-06-16 18:41:35 -0700

    saving

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,24 +14,23 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = i; j <= 99; j++)
 		{
-			if(i <= j)
+			int i_tens = i / 10;
+			int i_ones = i % 10;
+			int j_tens = j / 10;
+			int j_ones = j % 10;
+
+			putchar('0' + i_tens);
+			putchar('0' + i_ones);
+			putchar(' ');
+			putchar('0' + j_tens);
+			putchar('0' + j_ones);
+
+			if (i != 99 || j !=  99)
 			{
-				int i_tens = i / 10;
-				int i_ones = i % 10;
-				int j_tens = j / 10;
-				int j_ones = j % 10;
-				putchar('0' + i_tens);
-				putchar('0' + i_ones);
+				putchar(',');
 				putchar(' ');
-				putchar('0' + j_tens);
-				putchar('0' + j_ones);
-				if (i != 99 || j !=  99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
