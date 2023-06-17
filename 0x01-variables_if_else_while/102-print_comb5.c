9eec9fd7e1428c7545f8733259e44cb4e7ca4704
ernest-ayitey <ernestayitey65@gmail.com>
2023-06-16 18:05:17 -0700

    saving

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,31 +7,32 @@
  *
  * Return: Always 0(indicating Success)
  *
- */ 
+ */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = i; j <= 99; j++)
 		{
-			/* Calculate the tens and ones digits of i*/
 			int i_tens = i / 10;
 			int i_ones = i % 10;
-			/* Calculate the tens and ones digits of j*/
 			int j_tens = j / 10;
 			int j_ones = j % 10;
-			/* Print the combination of numbers*/
-		       	putchar('0' + i_tens);
+
+			putchar('0' + i_tens);
 			putchar('0' + i_ones);
 			putchar(' ');
 			putchar('0' + j_tens);
 			putchar('0' + j_ones);
-			putchar(',');
-			putchar(' ');
+
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-	putchar('\n');
-	return 0;
+       	return (0);
 }
