28db93a828e62d63fe34ca5452b43007ac2eaa30
ernest-ayitey <ernestayitey65@gmail.com>
2023-06-20 18:52:12 -0700

    saving

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main"
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for (leter = 'a'; letter <= 'Z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
