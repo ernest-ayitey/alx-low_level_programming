d73b61aa709271ef272662f95574e747e08a42e4
ernest-ayitey <ernestayitey65@gmail.com>
2023-06-21 04:55:39 +0000

    saving

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_times_tables - prints the times table for n.
+ * print_times_table - prints the times table for n.
  * @n: The multiplication table requested.
  * Return: Nothing.
  */
-void print_times_tables(int n)
+void print_times_table(int n)
 {
 	int i, j, res;
 
