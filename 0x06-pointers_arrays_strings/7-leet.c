a8b879583beafc89d69a97d191831242c89c0cee
ernest-ayitey <ernestayitey65@gmail.com>
2023-06-29 22:10:14 +0000

    saving

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,34 +9,31 @@
  *
  * Return: Always 0.
  */
+#include <stdlib.h>
 
 char *leet(char *str) {
 	char *result = (char *)malloc(sizeof(char));
-
 	int i = 0;
 	int j = 0;
 
 	while (str[i] != '\0') {
 		if (str[i] == 'a' || str[i] == 'A') {
-			result[j] = '4';
+			_putchar('4');
 		} else if (str[i] == 'e' || str[i] == 'E') {
-			result[j] = '3';
+			_putchar('3');
 		} else if (str[i] == 'o' || str[i] == 'O') {
-			result[j] = '0';
+			_putchar('0');
 		} else if (str[i] == 't' || str[i] == 'T') {
-			result[j] = '7';
+			_putchar('7');
 		} else if (str[i] == 'l' || str[i] == 'L') {
-			result[j] = '1';
+			_putchar('1');
 		} else {
-			result[j] = str[i];
+			_putchar(str[i]);
 		}
 
 		i++;
-		j++;
-		result = (char *)realloc(result, (j + 1) * sizeof(char));
 	}
 
-	result[j] = '\0';
 	return result;
 }
 
