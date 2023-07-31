327037c270a635665bfd0239283fa7260614a5d7
ernest-ayitey <ernestayitey65@gmail.com>
2023-07-31 14:32:20 +0000

    list file

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct_listint_s - singly linked list
+ * @i: integer
+ * @next: singly linked list node structure
+ * Description: singly linked list node structure
+ *
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}
+listint_t
+size_t print_listint(const listint_t *h);	
+#endif
