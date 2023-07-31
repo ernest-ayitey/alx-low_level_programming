ed310e20ec15f7fe3f83c505b7f6b641453cf70f
ernest-ayitey <ernestayitey65@gmail.com>
2023-07-31 19:09:57 +0000

    saving

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - Returns numbers of elements 
+ * @head: listint  head
+ * Return: number  of nodes
+ */
+size_t listint_len(const listint_t *h)
+
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
