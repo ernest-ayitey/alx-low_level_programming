a8cb34cf831745b0a980db0bddb113539d5a9d72
ernest-ayitey <ernestayitey65@gmail.com>
2023-08-01 15:49:32 +0000

    updating

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,7 +8,8 @@ int sum_listint(listint_t *head)
 {
 	int sum;
 
-	for (sum = 0; head; sum += head->n,head = head->next);
+	for (sum = 0; head; sum += head->n,
+		       	head = head->next);
 
 	return  (sum);
 }
