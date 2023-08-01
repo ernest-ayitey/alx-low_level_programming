ec76463aaec98cb6289693751354f34fb3789a15
ernest-ayitey <ernestayitey65@gmail.com>
2023-08-01 12:01:36 +0000

    updating 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	if (h == NULL)
+		return(0);
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
