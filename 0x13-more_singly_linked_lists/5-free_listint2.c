b9ffc82915fb56360f80055608c483c3ce83e531
ernest-ayitey <ernestayitey65@gmail.com>
2023-08-01 14:41:26 +0000

    function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * free_listint2(listint_t **head)
+ * free_listint2 - frees the list
  * @head: Address of the first node of a list
  */
 void free_listint2(listint_t **head)
