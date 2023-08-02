7b85b57a8c9568d4ec7754218081c539ea8a328e
ernest-ayitey <ernestayitey65@gmail.com>
2023-08-02 14:52:36 +0000

    fixing error

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 /**
- * sum_listint - calcultes the sum of all the data in a listint_t list
- * @head: first node in the linkrd list
+ * sum_listint - calcultes the sum of all the data
+ * in a listint_t list
+ * @head: first node in the linked list
  * Return: resulting sum
  */
 int sum_listint(listint_t *head)
 {
 	int sum;
 
-	for (sum = 0; head; sum += head->n,
-		       	head = head->next);
-
+	for (sum = 0; head; sum += head->n)
+	{
+		head = head->next;
+	}
 	return  (sum);
 }
