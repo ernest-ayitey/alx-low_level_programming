66ef2d07d25033921dc667b686a6d9ba6ebb7514
ernest-ayitey <ernestayitey65@gmail.com>
2023-08-07 22:40:03 +0000

    fixing bug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,23 +12,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int len = strlen(text_content);
-	FILE *fp = fopen(filename, "a");
+	int fd, len, w;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
 	if (text_content == NULL)
 	{
-		return (-1);
+		close(fd);
+		return (1);
 	}
-	if (fp == NULL)
-	{
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	w = write(fd, text_content, len);
+	if (w < 1)
 		return (-1);
-	}
-	fwrite(text_content, 1, len, fp);
-
-	fclose(fp);
+	close(fd);
 	return (1);
 }
